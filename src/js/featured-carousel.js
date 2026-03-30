8b7d436b1c143070674d84bb319328a204b22fb0
Arnold-nava <arnoldnava995@gmail.com>
2026-03-31 00:33:50 +0800

    try

---
+++ src/js/featured-carousel.js
@@ -0,0 +1,17 @@
+const count = document.querySelector('.wrapper');
+const card = document.querySelectorAll('.featured-card');
+const next = document.querySelector('.btn-right');
+const prev = document.querySelector('.btn-left');
+
+let current = 0;
+
+next.addEventListener('click', () => {
+    current = (current + 1) % card.length;
+    const cardWidth = card[0].offsetWidth + 20;
+    count.style.transform = `translateX(-${current * cardWidth}px)`;
+});
+
+prev.addEventListener('click', () => {
+    current = (current - 1 + card.length) % card.length;
+    count.style.transform = `translateX(-${current * 100}%)`;
+});
