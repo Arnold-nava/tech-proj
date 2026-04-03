427167f1afd4929e640d5f26a8b7dc016b70eff9
Arnold-nava <arnoldnava995@gmail.com>
2026-04-03 23:00:55 +0800

    added loader

---
+++ src/js/loader.js
@@ -0,0 +1,10 @@
+const video = document.querySelector(`.video`);
+const loader = document.querySelector(`.loader`);
+
+
+video.addEventListener(`ended` ,() => {
+    loader.style.animation = "smoother 1s linear";
+    loader.style.opacity ="0";
+
+    window.scrollY.false;
+})
